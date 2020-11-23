4e93cee883190398c146fb9dcba147172d6822b4
yujiro-baba <babajohnyujiro@gmail.com>
2020-11-24 02:31:23 +0900

    signupアクションを追加

---
+++ views/account/signup.php
@@ -0,0 +1,29 @@
+<!--レイアウトファイルに渡す変数を指定-->
+<?php $this->setLayoutVar('title', 'アカウント登録') ?>
+
+<h2>アカウント登録</h2>
+
+<form action="<?php echo $base_url; ?>/account/register" method="post">
+	<input type="hidden" name="_token" value="<?php echo $this->escape($_token); ?>" />
+
+	<table>
+		<tbody>
+			<tr>
+				<th>ユーザーID</th>
+				<td>
+					<input type="text" name="user_name"　value="" />
+				</td>
+			</tr>
+			<tr>
+				<th>パスワード</th>
+				<td>
+					<input type="password" name="password"　value="" />
+				</td>
+			</tr>
+		</tbody>
+	</table>
+
+	<p>
+		<input type="submit" value="登録">
+	</p>
+</form>
